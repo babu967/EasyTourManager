b64664c2b70542a92070beae1a3153557766b0ce
Mohiuddin Babu <babumd11@gmail.com>
2018-12-04 19:16:27 +0600

    Add Maps data

--- app/src/main/java/com/codex/easytourmanager/MapsActivity.java
+++ app/src/main/java/com/codex/easytourmanager/MapsActivity.java
@@ -1,27 +1,100 @@
 package com.codex.easytourmanager;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
+import android.os.Looper;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.codex.easytourmanager.maps_data.GetNearbyPlacesData;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private FusedLocationProviderClient client;
+    private LocationRequest request;
+    private LocationCallback callback;
+    private double lat, lng;
+    private GoogleMapOptions googleMapOptions;
+
+
+
+    int PROXIMITY_RADIUS = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        googleMapOptions = new GoogleMapOptions()
+                .zoomControlsEnabled(true)
+                .compassEnabled(true);
+
+
+
+
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkPermission();
+        }
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        intLocation();
+
+
+    }
+
+
+
+    private void intLocation() {
+        client = LocationServices.getFusedLocationProviderClient(this);
+        request = new LocationRequest();
+        // request.setInterval(5000); //auto update location after milli seconds.
+        request.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        callback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                for (Location location : locationResult.getLocations()) {
+                    lat = location.getLatitude();
+                    lng = location.getLongitude();
+
+                }
+                LatLng sydney = new LatLng(lat, lng);
+                mMap.addMarker(new MarkerOptions().position(sydney).title("your place"));
+                //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                // mMap.animateCamera(CameraUpdateFactory.zoomBy(14));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 18));
+            }
+        };
     }
 
 
@@ -39,8 +112,180 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+       /* LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            client.requestLocationUpdates(request, callback, Looper.getMainLooper());
+            intLocation();
+            // updateLocation();
+        }
+
+    }
+
+    public void onClick(View view) {
+        {
+            Object dataTransfer[] = new Object[2];
+            GetNearbyPlacesData getNearbyPlacesData = new GetNearbyPlacesData();
+
+
+
+            switch (view.getId()) {
+                case R.id.B_search:
+                    EditText txtPlace = findViewById(R.id.TF_location);
+                    String location = txtPlace.getText().toString();
+                    List<Address> addressList;
+
+                    if (location != "") {
+                        Geocoder geocoder = new Geocoder(this);
+                        try {
+                            addressList = geocoder.getFromLocationName(location, 5);
+
+                            if (addressList != null) {
+
+                                for (int i = 0; i < addressList.size(); i++) {
+
+                                    LatLng latLng = new LatLng(addressList.get(i).getLatitude(), addressList.get(i).getLongitude());
+                                    MarkerOptions markerOptions = new MarkerOptions();
+                                    markerOptions.position(latLng);
+                                    markerOptions.title(location);
+                                    mMap.addMarker(markerOptions);
+                                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                                    mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+                                }
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+
+                case R.id.B_hospital: {
+
+                    mMap.clear();
+                    String Hospital = "hospital";
+                    String url = getUrl(lat, lng, Hospital);
+
+                    dataTransfer[0] = mMap;
+                    dataTransfer[1] = url;
+
+                    getNearbyPlacesData.execute(dataTransfer);
+                    Toast.makeText(MapsActivity.this, "Showing Nearby Hospitals", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                case R.id.B_Cafe: {
+
+                    mMap.clear();
+                    String Cafe = "cafe";
+                    String url = getUrl(lat, lng, Cafe);
+
+                    dataTransfer[0] = mMap;
+                    dataTransfer[1] = url;
+
+                    getNearbyPlacesData.execute(dataTransfer);
+                    Toast.makeText(MapsActivity.this, "Showing Nearby Cafe", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                case R.id.B_restaurant: {
+                    mMap.clear();
+                    String Restaurant = "restaurant";
+                    String url = getUrl(lat, lng, Restaurant);
+
+                    dataTransfer[0] = mMap;
+                    dataTransfer[1] = url;
+
+                    getNearbyPlacesData.execute(dataTransfer);
+                    Toast.makeText(MapsActivity.this, "Showing Nearby Restaurants", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                case R.id.B_Atm: {
+                    mMap.clear();
+                    String Atm = "atm";
+                    String url = getUrl(lat, lng, Atm);
+
+                    dataTransfer[0] = mMap;
+                    dataTransfer[1] = url;
+
+                    getNearbyPlacesData.execute(dataTransfer);
+                    Toast.makeText(MapsActivity.this, "Showing Nearby Atm", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                case R.id.B_Hotel: {
+                    mMap.clear();
+                    String Hotel = "hotel";
+                    String url = getUrl(lat, lng, Hotel);
+
+                    dataTransfer[0] = mMap;
+                    dataTransfer[1] = url;
+
+                    getNearbyPlacesData.execute(dataTransfer);
+                    Toast.makeText(MapsActivity.this, "Showing Nearby Hotels", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+            }
+        }
+    }
+
+
+    public void updateLocation() {
+
+        request = new LocationRequest();
+        request.setInterval(100);
+        request.setFastestInterval(1000);
+        request.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        client.requestLocationUpdates(request, callback, Looper.myLooper());
+
+    }
+
+
+    private String getUrl(double latitude, double longitude, String nearbyPlace) {
+
+        StringBuilder googlePlaceUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        googlePlaceUrl.append("location=" + latitude + "," + longitude);
+        googlePlaceUrl.append("&radius=" + PROXIMITY_RADIUS);
+        googlePlaceUrl.append("&type=" + nearbyPlace);
+        googlePlaceUrl.append("&sensor=true");
+        googlePlaceUrl.append("&key=" + "AIzaSyDjCDLVd8keFm-iPDss7aBb2fLe4Xaxk94");
+
+        Log.d("MapsActivity", "url = " + googlePlaceUrl.toString());
+
+        return googlePlaceUrl.toString();
+    }
+
+
+    private void checkPermission() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        client.removeLocationUpdates(callback);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPermission();
+        client.requestLocationUpdates(request, callback, Looper.getMainLooper());
+
     }
 }
